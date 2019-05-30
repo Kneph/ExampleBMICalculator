35d1d81788c591309c859707121e703113db24ca
Katri Vilonen <katri.vilonen@gmail.com>
2019-05-30 14:34:46 +0300

    removed android.R import

--- app/src/main/java/com/example/examplebmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/examplebmicalculator/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.examplebmicalculator
 
-import android.R
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
@@ -11,24 +10,23 @@ class MainActivity : AppCompatActivity() {
 
     val heightEditText = findViewById<View>(R.id.etxt_height) as EditText
     val weightEditText = findViewById<View>(R.id.etxt_weight) as EditText
-    val resultViev = findViewById<View>(R.id.result) as TextView
+    val resultViev = findViewById<View>(R.id.txt_result) as TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        fun calculateBMI(v: View) {
+    fun calculateBMI(v: View) {
 
-            if (!heightEditText.text.toString().isNullOrEmpty() && !weightEditText.text.toString().isNullOrEmpty()) {
+        if (!heightEditText.text.toString().isNullOrEmpty() && !weightEditText.text.toString().isNullOrEmpty()) {
 
-                val height = java.lang.Double.parseDouble(heightEditText.text.toString()) / 100
-                val weight = java.lang.Double.parseDouble(weightEditText.text.toString())
+            val height = java.lang.Double.parseDouble(heightEditText.text.toString()) / 100
+            val weight = java.lang.Double.parseDouble(weightEditText.text.toString())
 
+            val bmi = weight / (height * height)
+            resultViev.text = bmi.toString()
 
-                val bmi = weight / (height * height)
-                resultViev.text = bmi.toString()
-
-            }
         }
     }
 }
